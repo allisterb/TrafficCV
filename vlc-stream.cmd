c7e1ca5416343366393ff78ca0a4bbf82f1141d2
Allister Beharry <allister.beharry@gmail.com>
2020-11-22 14:30:04 -0400

    On Windows use the full path to default VLC location.

--- vlc-stream.cmd
+++ vlc-stream.cmd
@@ -1,3 +1,3 @@
 @echo off
-echo Streaming video source %1 as mpjpeg on port 18223
-vlc --no-audio %1 --screen-fps 1 --sout "#transcode{vcodec=MJPG,vb=800}:standard{access=http,mux=mpjpeg,dst=:18223/}" --sout-http-mime="multipart/x-mixed-replace;boundary=--"
+echo Streaming video source %1 as mpjpeg on port 18223 using VLC...
+"C:\Program Files\VideoLan\VLC\vlc" --no-audio %1 --screen-fps 1 --sout "#transcode{vcodec=MJPG,vb=800}:standard{access=http,mux=mpjpeg,dst=:18223/}" --sout-http-mime="multipart/x-mixed-replace; boundary=--"
